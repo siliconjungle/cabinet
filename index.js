f0bb1f0044805e7da908f62ccf31eab9f9d5d77b
siliconjungle <siliconjungle.io@gmail.com>
2022-02-12 10:51:34 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+import EventEmitter from 'events'
+import {
+  createShelf,
+  getDataFromShelf,
+  mergeShelves,
+  createLocalChangesDiff,
+} from './shelf.js'
+
+// Adding setMaxListeners(0) is potentially dangerous for memory leak reasons.
+const shelfEmitter = new EventEmitter()
+shelfEmitter.setMaxListeners(0)
+
+const emitter = new EventEmitter()
+emitter.setMaxListeners(0)
+
+// Should transclusion be a thing? Not sure if it's needed.
+const store = {}
+
+export const getState = (key) => store[key] && getDataFromShelf(store[key]) || null
+
+export const setState = (key, state) => {
+  store[key] = store[key] ? mergeShelves(store[key], createLocalChangesDiff(store[key], state)) : createShelf(state)
+  shelfEmitter.emit(
+    key,
+    key,
+    store[key],
+  )
+  if (emitter.listenerCount(key) > 0) {
+    emitter.emit(
+      key,
+      key,
+      getState(key),
+    )
+  }
+}
+
+export const getShelf = (key) => store[key] || null
+
+export const setShelf = (key, shelf) => {
+  store[key] = store[key] ? mergeShelves(store[key], shelf) : shelf
+  shelfEmitter.emit(
+    key,
+    key,
+    store[key],
+  )
+  if (emitter.listenerCount(key) > 0) {
+    emitter.emit(
+      key,
+      key,
+      getState(key),
+    )
+  }
+}
+
+export const addSubscription = (key, callback) => {
+  emitter.addListener(key, callback)
+}
+
+export const removeSubscription = (key, callback) => {
+  emitter.removeListener(key, callback)
+}
+
+export const addShelfSubscription = (key, callback) => {
+  shelfEmitter.addListener(key, callback)
+}
+
+export const removeShelfSubscription = (key, callback) => {
+  shelfEmitter.removeListener(key, callback)
+}
---
+++ shelf.js
@@ -0,0 +1,183 @@
+const isObj = data =>
+  typeof data && typeof data === 'object' && !Array.isArray(data)
+
+export const compareShelves = (shelf, shelf2) => {
+  if (shelf) {
+    if (shelf2) {
+      if (shelf[1] > shelf2[1]) {
+        return 1
+      } else if (shelf2[1] > shelf[1]) {
+        return -1
+      }
+      if (isObj(shelf[0])) {
+        if (isObj(shelf2[0])) {
+          return 0
+        }
+        return 1
+      } else if (isObj(shelf2[0])) {
+        return -1
+      }
+      // Can something more explicit be added here?
+      // This relies too heavily on JS implementation of stringify.
+      // This makes it harder for other languages to be interoperable.
+      const shelfJson = JSON.stringify(shelf[0])
+      const shelf2Json = JSON.stringify(shelf2[0])
+      if (shelfJson === shelf2Json) {
+        return 0
+      }
+      return shelfJson < shelf2Json ? 1 : -1
+    }
+    return 1
+  }
+  if (shelf2) {
+    return -1
+  }
+  return 0
+}
+
+export const createShelf = (data, objectVersion = 0) => {
+  return (
+    [
+      isObj(data) ?
+        Object.entries(data).reduce((p, [k, v]) => {
+          return ({
+            ...p,
+            [k]: isObj(v) ? createShelf(v) : [v, 0],
+          })
+        }, {}) :
+        data,
+      objectVersion,
+    ]
+  )
+}
+
+export const getDataFromShelf = shelf => {
+  return isObj(shelf[0]) ? (
+    Object.entries(shelf[0]).reduce((p, [k, v]) => {
+      if (v[0] === null) {
+        return {
+          ...p,
+        }
+      }
+      return {
+        ...p,
+        [k]: isObj(v[0]) ? getDataFromShelf(v) : v[0],
+      }
+    }, {})
+  ) : shelf[0]
+}
+
+// This diff creates a shelf that contains the changes which would be applied for a merge.
+export const diffShelves = (shelf, shelf2) => {
+  // Only return fields that belong to 2
+  // Or that belong to 1 and don't belong to 2
+  if (!shelf && !shelf2) {
+    return null
+  }
+  const shelfOrder = compareShelves(shelf, shelf2)
+  if (shelfOrder === 1) {
+    return null
+  } else if (shelfOrder === -1) {
+    return shelf2
+  }
+
+  if (JSON.stringify(shelf[0]) === JSON.stringify(shelf2[0])) {
+    return [null, shelf2[1]]
+  }
+
+  const keys = [
+    ...new Set(
+      [
+        ...Object.keys(shelf[0]),
+        ...Object.keys(shelf2[0])
+      ]
+    )
+  ]
+  return [
+    keys.reduce((p, key) => {
+      const shelfOrder2 = compareShelves(shelf[0][key], shelf2[0][key])
+      if (shelfOrder2 === 1 || JSON.stringify(shelf[0][key]) === JSON.stringify(shelf2[0][key])) {
+        return {
+          ...p,
+        }
+      }
+      return {
+        ...p,
+        [key]: diffShelves(shelf[0][key] || null, shelf2[0][key] || null),
+      }
+    }, {}),
+    shelf[1]
+  ]
+}
+
+export const mergeShelves = (shelf, shelf2) => {
+  if (!shelf && !shelf2) {
+    return null
+  }
+  const shelfOrder = compareShelves(shelf, shelf2)
+  if (shelfOrder === 1) {
+    return shelf
+  } else if (shelfOrder === -1) {
+    return shelf2
+  }
+
+  if (JSON.stringify(shelf[0]) === JSON.stringify(shelf2[0])) {
+    return shelf2
+  }
+
+  const keys = [
+    ...new Set(
+      [
+        ...Object.keys(shelf[0]),
+        ...Object.keys(shelf2[0]),
+      ]
+    )
+  ]
+  return [
+    keys.reduce((p, key) => {
+      return {
+        ...p,
+        [key]: mergeShelves(shelf[0][key] || null, shelf2[0][key] || null),
+      }
+    }, {}),
+    shelf[1],
+  ]
+}
+
+// Apply local changes
+// Loop through all keys
+// If key belongs to shelf then set data's version to shelf +1
+// If it doesn't, set data's version to 0
+// Perhaps a better name is createLocalChangesDiff?
+export const createLocalChangesDiff = (shelf, data) => {
+  if (!shelf) {
+    return [data, 0]
+  }
+
+  if (!isObj(data)) {
+    return shelf[0] === data ? [data, shelf[1]] : [data, shelf[1] + 1]
+  }
+
+  if (!isObj(shelf[0])) {
+    return createShelf(data, shelf[1] + 1)
+  }
+
+  const keys = [
+    ...new Set(
+      [
+        ...Object.keys(shelf[0]),
+        ...Object.keys(data),
+      ]
+    )
+  ]
+
+  return [
+    keys.reduce((p, key) => {
+      return {
+        ...p,
+        [key]: createLocalChangesDiff(shelf[0][key] || null, data[key] || null),
+      }
+    }, {}),
+    shelf[1],
+  ]
+}
