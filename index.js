1f5b70f549e14d012da509d1631713bf52119c5f
siliconjungle <siliconjungle.io@gmail.com>
2022-03-22 20:06:05 +1100

    make one event listener

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ import {
 function Index (repository) {
   this.emitter = new EventEmitter()
   this.emitter.setMaxListeners(0)
-  this.shelfEmitter = new EventEmitter()
-  this.shelfEmitter.setMaxListeners(0)
   this.repository = repository
   this.store = {}
 }
@@ -47,51 +45,44 @@ Index.prototype.setState = async function (key, state) {
   if (shelf) {
     const localChanges = getLocalChanges(shelf.value, state, shelf.versions)
     const appliedOps = applyOps(shelf, localChanges)
-    this.setShelf(key, appliedOps)
+    this.setShelf(key, appliedOps, localChanges)
+    return localChanges
   } else {
     shelf = createShelf(state)
-    this.setShelf(key, shelf)
+    this.setShelf(key, shelf, shelf.history)
+    return shelf.history
   }
 }
 
-Index.prototype.setShelf = function(key, shelf) {
+// I actually think you could entirely remove one of them
+Index.prototype.setShelf = function(key, shelf, ops) {
   this.store[key] = shelf
 
   // Currently not handling success / failure.
   this.repository.setShelfByKey(key, shelf)
 
-  this.shelfEmitter.emit(
+  this.emitter.emit(
     key,
     key,
     shelf,
+    ops,
   )
-  if (this.emitter.listenerCount(key) > 0) {
-    this.emitter.emit(
-      key,
-      key,
-      shelf.value,
-    )
-  }
 }
 
 Index.prototype.applyOp = async function(key, op) {
   let shelf = await this.getShelf(key)
-  this.setShelf(key, applyOp(shelf || createShelf(), op))
+  this.setShelf(key, applyOp(shelf || createShelf(), op), [op])
 }
 
 Index.prototype.applyOps = async function(key, ops) {
-  console.log('_KEY_', key)
-  console.log('_OPS_', ops)
   const shelf = await this.getShelf(key)
-  console.log('_CURRENT_SHELF_', shelf)
-  this.setShelf(key, applyOps(shelf || createShelf(), ops))
+  this.setShelf(key, applyOps(shelf || createShelf(), ops), ops)
 }
 
 Index.prototype.initShelf = async function(key) {
-  console.log('_INIT_SHELF_', key)
   const shelf = await this.repository.getShelfByKey(key)
   if (shelf) {
-    this.setShelf(key, shelf)
+    this.setShelf(key, shelf, shelf.history)
   }
 }
 
@@ -104,31 +95,13 @@ Index.prototype.addSubscription = function(key, callback) {
 
 Index.prototype.removeSubscription = function(key, callback) {
   this.emitter.removeListener(key, callback)
-  if (this.emitter.listenerCount(key) === 0 && this.shelfEmitter.listenerCount(key) === 0) {
+  if (this.emitter.listenerCount(key) === 0) {
     this.removeShelf(key)
   }
 }
 
 Index.prototype.SubscriptionCount = function(key) {
   this.emitter.listenerCount(key)
-}
-
-Index.prototype.addShelfSubscription = function(key, callback) {
-  if (!this.store[key]) {
-    this.initShelf(key)
-  }
-  this.shelfEmitter.addListener(key, callback)
-}
-
-Index.prototype.removeShelfSubscription = function(key, callback) {
-  this.shelfEmitter.removeListener(key, callback)
-  if (this.emitter.listenerCount(key) === 0 && this.shelfEmitter.listenerCount(key) === 0) {
-    this.removeShelf(key)
-  }
-}
-
-Index.prototype.getShelfSubscriptionCount = function(key) {
-  this.shelfEmitter.listenerCount(key)
 }
 
 export default Index
